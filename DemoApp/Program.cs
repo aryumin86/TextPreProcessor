223038a598f2d8f298eedab34c21f09105da783d
Alexey Ryumin <aryumin@aryumin.ru>
2017-08-21 22:29:47 +0300

    added demo app

---
+++ DemoApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DemoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
