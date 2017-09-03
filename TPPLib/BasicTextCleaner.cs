e76c8c032bae31e9ac529238ef03d58ef9800f1b
Alexey Ryumin <aryumin@aryumin.ru>
2017-09-03 14:54:56 +0300

    Added more methods for basic text cleaner

--- TPPLib/BasicTextCleaner.cs
+++ TPPLib/BasicTextCleaner.cs
@@ -8,13 +8,74 @@ namespace TPPLib
 {
     /// <summary>
     /// Класс базовой очистки текстов.
+    /// Думал, что здесь будет значительно методов, но,видимо,
+    /// нередко может пригодится сделать очистку текстов с помощью этого
+    /// класса.
     /// </summary>
     public class BasicTextCleaner
     {
+        /// <summary>
+        /// Базовая очистка текста.
+        /// </summary>
+        /// <param name="raw"></param>
         public void MakeBasicCleaning(RawText raw){
             raw.Content = raw.Content.ToLower();
             raw.Content = Regex.Replace(raw.Content, 
                 @"\s+", " ", RegexOptions.Compiled | RegexOptions.Singleline);
+        }
+
+        /// <summary>
+        /// Удаление email.
+        /// </summary>
+        /// <param name="raw"></param>
+        public void RemoveEmails(RawText raw)
+        {
+
+        }
+
+        /// <summary>
+        /// Удаление хеш-тегов.
+        /// </summary>
+        /// <param name="raw"></param>
+        public void RemoveHashTags(RawText raw)
+        {
+
+        }
+
+        /// <summary>
+        /// Удаление урлов.
+        /// </summary>
+        /// <param name="raw"></param>
+        public void RemoveUrls(RawText raw)
+        {
+
+        }
+
+        /// <summary>
+        /// Удаление html и js тегов.
+        /// </summary>
+        /// <param name="raw"></param>
+        public void RemoveHtmlAndJsTags(RawText raw)
+        {
+
+        }
+
+        /// <summary>
+        /// Удаление пунктуации.
+        /// </summary>
+        /// <param name="raw"></param>
+        public void RemovePunctuation(RawText raw)
+        {
+
+        }
+
+        /// <summary>
+        /// Удаление одиночно стоящих цифр.
+        /// </summary>
+        /// <param name="raw"></param>
+        public void RemoveSingleStaingNumbers(RawText raw)
+        {
+
         }
 
         public IEnumerable<Token> MakeBasicCleaning(IEnumerable<Token> tokens){
